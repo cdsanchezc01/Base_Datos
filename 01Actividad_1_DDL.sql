56755a2c6a47d33403a1762604b92624138b187f
cdsanchezc01 <cdsanchezc01@educarex.es>
2024-03-11 14:29:02 +0100

    1103

--- 01Actividad_1_DDL.sql
+++ 01Actividad_1_DDL.sql
@@ -4,16 +4,57 @@ USE Actividad_1;
 DROP TABLE producto;
 
 CREATE TABLE Producto (
-    codigo INT PRIMARY KEY, -- es unica y not null
-    nombre VARCHAR(150) not null,
-    precio DOUBLE not null
+    codigo INT PRIMARY KEY,
+    nombre VARCHAR(150) NOT NULL,
+    precio DOUBLE NOT NULL
 );
+drop TABLE if exists  Cliente;
 
 CREATE TABLE Cliente (
-    cliente_ID VARCHAR(150) PRIMARY KEY,
-    Dni VARCHAR(9) NOT NULL,
-    nombre VARCHAR(150),
-    apellido1 VARCHAR(100),
-    apellido2 VARCHAR(100) NOT NULL
+    ID_Ciente INT,
+    Dni VARCHAR(9) NOT NULL UNIQUE,
+    nombre VARCHAR(150) NOT NULL,
+    apellido1 VARCHAR(100) NOT NULL,
+    apellido2 VARCHAR(100),
+    CONSTRAINT PK_Cliente PRIMARY KEY (ID_Ciente),
+    CONSTRAINT UQ_Cliente_Dni UNIQUE (Dni)
 );
+
+CREATE TABLE LIBRO (
+    ID_Libro INT,
+    contenido BLOB NOT NULL,
+    CONSTRAINT PK_LIBRO PRIMARY KEY (ID_Libro)
+    -- SE pone asi los primary key 
+);
+-- Recuerda los atributos discontinuos se quedan así porque admite nulos pero el normal se pone not null
+-- Empleado (DNI, nombre, ap1, ap2, email, tlfo, fechaNac, rol, ultimoAcceso)
+CREATE TABLE Empleado (
+    DNI VARCHAR(9),
+    nombre VARCHAR(150) NOT NULL,
+    apellido1 VARCHAR(100) NOT NULL,
+    apellido2 VARCHAR(100),
+    telefono VARCHAR(12) NOT NULL,
+    email VARCHAR(200),
+    fecha_nacimiento DATE NOT NULL,
+    rol ENUM('PJ', 'PS', 'AF', 'AP') NOT NULL,
+    ultimo_acceso DATETIME NOT NULL,
+    CONSTRAINT PK_Empleado PRIMARY KEY (DNI)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- 01Actividad_1_DML.sql
+++ 01Actividad_1_DML.sql
@@ -1,4 +1,6 @@
 use Actividad_1;
 
 insert into producto(codigo, nombre, precio) values(1,"martillo", 20.5);
-insert into producto(codigo, nombre, precio) values(1,"martillo", 20.5);
+insert into Libro (id_libro, contenido) value (1, "data:text/plain;base64,SE9MQSBNVU5ETw==");
+ -- insert into Empleado (DNI, nombre, ap1) corrigelo 
+ 
