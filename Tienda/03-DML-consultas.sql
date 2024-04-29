f3e12da7149c97079e700b61b2c307cb6019ef4c
cdsanchezc01 <cdsanchezc01@educarex.es>
2024-04-29 14:09:48 +0200

    2904

--- Tienda/03-DML-consultas.sql
+++ Tienda/03-DML-consultas.sql
@@ -12,63 +12,145 @@ select nombre, precio from producto;
 select nombre, precio 'precio_euro', roud((precio*1.07), 2) 'precio_USD' from producto;
 
 -- Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a mayúscula.
-
+select upper(nombre), precio from producto;
+ 
 -- Lista los nombres y los precios de todos los productos de la tabla producto, convirtiendo los nombres a minúscula.
+select lower(nombre), precio from producto;
 
--- Lista el nombre de todos los fabricantes en una columna, y en otra columna obtenga en mayúsculas los dos primeros caracteres del nombre del fabricante.
+-- Lista el nombre de todos los fabricantes en una columna, 
+-- y en otra columna obtenga en mayúsculas los dos primeros caracteres del nombre del fabricante.
+select nombre, upper(substring(nombre,1,2)) from fabricante;
 
 -- Lista los nombres y los precios de todos los productos de la tabla producto, redondeando el valor del precio.
+select nombre, round(precio) 'precio_ordenado' from producto;
 
 -- Lista los nombres y los precios de todos los productos de la tabla producto, truncando el valor del precio para mostrarlo sin ninguna cifra decimal.
+select nombre, floor(precio) 'precio_ordenado' from producto; -- redondeo a la baja
+select nombre, ceil(precio) 'precio_ordenado' from producto; -- redondea al alza
 
 -- Lista el identificador de los fabricantes que tienen productos en la tabla producto.
+select id_fabricante from producto;
 
 -- Lista el identificador de los fabricantes que tienen productos en la tabla producto, eliminando los identificadores que aparecen repetidos.
+select distinct id_fabricante from producto;
 
 -- Lista los nombres de los fabricantes ordenados de forma ascendente.
+select nombre from fabricante order by nombre asc;
 
 -- Lista los nombres de los fabricantes ordenados de forma descendente.
+select nombre from fabricante order by nombre desc;
 
 -- Lista los nombres de los productos ordenados en primer lugar por el nombre de forma ascendente y en segundo lugar por el precio de forma descendente.
+select nombre, precio from producto order by nombre asc, precio desc;
 
 -- Devuelve una lista con las 5 primeras filas de la tabla fabricante.
+select * from fabricante limit 5;
 
--- Devuelve una lista con 2 filas a partir de la cuarta fila de la tabla fabricante. La cuarta fila también se debe incluir en la respuesta.
+-- Devuelve una lista con 2 filas a partir de la cuarta fila de la tabla fabricante. 
+-- La cuarta fila también se debe incluir en la respuesta.
+select * from fabricante limit 2 offset 3;
 
 -- Lista el nombre y el precio del producto más barato. (Utilice solamente las cláusulas ORDER BY y LIMIT)
+SELECT 
+    nombre, precio
+FROM
+    producto
+ORDER BY precio ASC
+LIMIT 1;
 
 -- Lista el nombre y el precio del producto más caro. (Utilice solamente las cláusulas ORDER BY y LIMIT)
+SELECT 
+    nombre, precio
+FROM
+    producto
+ORDER BY precio desc
+LIMIT 1;
 
 -- Lista el nombre de todos los productos del fabricante cuyo identificador de fabricante es igual a 2.
-
+SELECT 
+    nombre
+FROM
+    producto
+WHERE
+    id_fabricante = 2;
+    
 -- Lista el nombre de los productos que tienen un precio menor o igual a 120€.
+SELECT nombre
+FROM producto
+WHERE precio <= 120;
 
 -- Lista el nombre de los productos que tienen un precio mayor o igual a 400€.
+SELECT nombre
+FROM producto
+WHERE precio >= 400;
 
 -- Lista el nombre de los productos que no tienen un precio mayor o igual a 400€.
+SELECT nombre
+FROM producto
+WHERE precio < 400;
 
 -- Lista todos los productos que tengan un precio entre 80€ y 300€. Sin utilizar el operador BETWEEN.
+select nombre, precio
+from producto 
+where precio >= 80 and precio <= 300;
 
 -- Lista todos los productos que tengan un precio entre 60€ y 200€. Utilizando el operador BETWEEN.
+select *
+from producto 
+where precio between 60 and 200;
 
 -- Lista todos los productos que tengan un precio mayor que 200€ y que el identificador de fabricante sea igual a 6.
+select *
+from producto
+where precio > 200 and id_fabricante = 6; 
 
 -- Lista todos los productos donde el identificador de fabricante sea 1, 3 o 5. Sin utilizar el operador IN.
+select * 
+from producto
+where id_fabricante = 1 or id_fabricante = 3 or id_fabricante = 5;
 
 -- Lista todos los productos donde el identificador de fabricante sea 1, 3 o 5. Utilizando el operador IN.
+select * 
+from producto
+where id_fabricante in (1,3,5);
 
 -- Lista el nombre y el precio de los productos en céntimos (Habrá que multiplicar por 100 el valor del precio). Cree un alias para la columna que contiene el precio que se llame céntimos.
+select nombre, precio (precio*100) 'centimos'
+from producto;
 
 -- Lista los nombres de los fabricantes cuyo nombre empiece por la letra S.
+select nombre 
+from fabricante 
+where left(nombre, 1) = 'S';
+
+select nombre 
+from fabricante 
+where nombre like ('S%');  -- % cualquier caracter
 
 -- Lista los nombres de los fabricantes cuyo nombre termine por la vocal e.
+select nombre 
+from fabricante 
+where right(nombre, 1) = 'e';
 
 -- Lista los nombres de los fabricantes cuyo nombre contenga el carácter w.
+select nombre 
+from fabricante 
+where nombre like ('%w%') or nombre like ('%w') or nombre like ('w%');  -- % cualquier caracter
 
 -- Lista los nombres de los fabricantes cuyo nombre sea de 4 caracteres.
+select nombre 
+from fabricante 
+where char_length(nombre) = 4;  
 
 -- Devuelve una lista con el nombre de todos los productos que contienen la cadena Portátil en el nombre.
+select nombe from producto where nombre like '%portatil%';
 
--- Devuelve una lista con el nombre de todos los productos que contienen la cadena Monitor en el nombre y tienen un precio inferior a 215 €.
+-- Devuelve una lista con el nombre de todos los productos que contienen la cadena Monitor en el nombre 
+-- y tienen un precio inferior a 215 €.
+select nombe from producto where nombre like '%monitor%' and precio > 215;
 
 -- Lista el nombre y el precio de todos los productos que tengan un precio mayor o igual a 180€. Ordene el resultado en primer lugar por el precio (en orden descendente) y en segundo lugar por el nombre (en orden ascendente).
+select nombre
+ from producto
+ where precio >= 180 
+ order by precio desc, nombre asc;
--- Unidad5/02-consultas.sql
+++ Unidad5/02-consultas.sql
@@ -107,6 +107,156 @@ select nombre, ape1, ape2 from empleado where ape2 is not null;
 
 
 
+-- nuevo ejercicio
+
+-- Contar el número total de empleados.
+select count(*) as empleado from empleado;
+
+-- Encontrar la edad máxima entre los empleados.
+select max(edad) as edad_maxima from empleado;
+
+-- Encontrar la edad mínima entre los empleados.
+select min(edad) as edad_minima from empleado;
+
+-- Calcular el salario promedio de los empleados. Redondeado a 2 decimales.
+select round(avg(salario),2) as salario_promedio from empleado;
+
+-- Contar el número de empleados por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+-- select departamento, count(*) as numero_empleados from empleadon
+
+-- Encontrar el salario máximo por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+select departamento,
+case 
+when departamento is null then 'Sin Dpto'
+else departamento
+end as departameto,
+max(salario) as salario_maximo
+from empleados
+group by departemento;
+
+-- Encontrar el salario mínimo por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+
+
+-- Calcular el salario promedio por departamento. Si el departamento es nulo, poner 'Sin Dpto'. Redondeado a 2 decimales
+
+
+-- Calcular el total de comisiones por departamento. Si el departamento es nulo, poner 'Sin Dpto'
+
+
+-- Encontrar el salario máximo de los empleados por departamento, siempre que el departamento no sea nulo. Ordenar de mayor a menor salario.
+
+
+-- Modifica la anterior consulta para sacar solo los 3 departamentos que mas ganan.
+
+
+-- Encontrar el salario máximo de los empleados por departamento, siempre que el departamento no sea nulo. Solo debe obtener aquellos máximos mayores de 50000. Ordenar de mayor a menor salario.
+
+
+-- Consulta de empleados obteniendo su nombre_completo y salario. Siempre que el salario sea mayor de 25000 ordena por nombre_completo.
+select concat(nombre,' ', ape1, ' ', ifnull(ape2, ' ')) as nombre_completo, salario from empleado where salario > 2500 order by nombre_completo;
+
+-- Consulta de empleados obteniendo su nombre_completo, edad, salario, siempre que estos tenga edad entre 20-30 años y su salario oscile entre 25000-32000 ordena de manera descendente por salario. Con between.
+select concat(nombre,' ', ape1, ' ', ifnull(ape2, ' ')) as nombre_completo, edad, salario from empleado where edad;
+
+-- Consulta de empleados obteniendo su nombre_completo, edad, salario, siempre que estos tenga edad entre 20-30 años y su salario oscile entre 25000-32000 ordena de manera descendente por salario. Sin between.
+select concat(nombre,' ', ape1, ' ', ifnull(ape2, ' ')) as nombre_completo, edad, salario from empleado where edad;
+
+-- Consulta todos los empleados (tanto internos como externos) mostrando nombre + 1 apellido + 2 apellido (en una columna llamada nombre completo), fecha_contratacion o subcontratacion, y tipo ('Interno o Subcontratado'). Quitando repetidos.
+-- Ordena por nombre_completo
+
+
+
+
+-- Consulta todos los empleados (tanto internos como externos) mostrando nombre + 1 apellido + 2 apellido (en una columna llamada nombre completo), fecha_contratacion o subcontratacion, y tipo ('Interno o Subcontratado'). Con repetidos.
+-- Ordena por nombre_completo
+
+
+
+
+-- Consulta los años que lleva cada empleado externo trabajando, mostrar el id, nombre, apellido1, fecha_subcontratacion y num años. Trunca a la baja el numero de años.
+SELECT 
+    id_empleado,
+    nombre,
+    apellido1,
+    fecha_subcontratacion,
+    YEAR(CURRENT_DATE) - YEAR(fecha_subcontratacion) AS num_anios
+FROM empleados_externos;
+
+
+
+
+-- Partiendo de la consulta anterior obten el que mas tiempo lleva trabajando. Solo debe devolver un registro
+
+SELECT id_empleado, nombre, apellido1, fecha_subcontratacion, YEAR(CURRENT_DATE) - YEAR(fecha_subcontratacion) AS num_anios FROM empleados_externos ORDER BY num_anios DESC LIMIT 1;
+
+
+
+-- Consulta cuantos empleados por departamento existen que tenga un salario de más de 40000 y que cobren comision.  Debe aparecer el id_departamento y el total por departamento
+SELECT id_departamento, COUNT(*) AS total_empleados FROM nombre_de_tabla WHERE salario > 40000 AND comisiones IS NOT NULL GROUP BY id_departamento;
+
+
+
+
+-- Consulta los datos de la venta realizada por el empleado 1 ordenados por fecha mas reciente
+
+
+
+
+-- Consulta los datos la ultima venta realizada por el empleado 1
+
+
+
+-- Consulta los datos de la ventas realizadas en abril
+
+
+
+-- Consulta los datos la maxima venta realizada en abril
+
+
+
+-- Cuenta el numero de ventas realizadas por el empleado 1
+
+
+
+-- Obten los empleados externos que nacieron 1981 
+
+
+
+-- Cuenta los empleados externos que nacieron en 1981
+
+
+
+-- Obten los empleados externos que nacieron en noviembre
+
+
+
+-- Obten maximo año de nacimiento de los empleados
+
+
+
+-- Obten los nombres y apellidos de los empleados (1 columna), la edad y 
+-- según el tramo de edad, es lo que debe valer plan:
+-- > 55 = Plan mayores de 55
+-- > 35 = Sin ventajas
+-- Resto = Plan joven 
+
+
+
+
+
+
+
+-- Haz lo mismo pero con los empleados externos.
+
+
+
+
+
+
+
+-- Haz lo mismo con los empleados interno y externo en la misma consulta, diferenciando cuales son internos o externos mediante una nueva columna.
+
+
 
 
 
