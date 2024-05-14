15dc395873e4820d26cb78d11fe76f72aed90a82
cdsanchezc01 <cdsanchezc01@educarex.es>
2024-05-14 14:28:53 +0200

    1407

--- Formatex/02-DML-Consultas.sql
+++ Formatex/02-DML-Consultas.sql
@@ -32,17 +32,33 @@ select p.*
 from profesor p
 where p.cod_dpto = (select d.cod_dpto from departamento d where d.nombre = 'Informatica');
 
-
 -- Muestra el nombre completo de los profesores que no tienen un segundo apellido.
 select concat_ws(' ', nombre, prApellido, sgApellido) as nombre_completo
 from profesor
 where sgApellido is null;
 
 -- Lista los módulos impartidos en el ciclo DAM.
-select m.nombre as nombre_modulo 
+select m.*
 from modulo m
 join ciclo c on m.cod_ciclo = c.cod_ciclo
-where c.sigla = 'DAM';   -- Recuerda preguntar por que se pone eso del DAM
+where c.sigla = 'DAM';
+
+-- Sub select ^
+select *
+from modulo 
+where cod_ciclo = (select cod_ciclo from ciclo where sigla = 'DAM');
+-- Consulta co-relacionada 
+select m.*
+from modulo m 
+where exists (select cod_ciclo from ciclo c where sigla = 'DAM' and m.cod_ciclo = c.cod_ciclo);
+-- Join con sub-select
+select modulo.*, aux.nombre, aux.cod_familia
+from modulo
+	join (select cod_ciclo, nombre, cod_familia
+		from ciclo 
+        where sigla = 'DAM'
+        ) aux on modulo.cod_ciclo = aux.cod_ciclo;
+   
 
 -- Encuentra los profesores que no están asignados a ningún departamento.
 select p.*
@@ -64,10 +80,14 @@ join departamento d on p.cod_dpto = d.cod_dpto
 group by d.nombre;
 
 -- De la consulta anterior, solo obten el que más numero de profesores tienen.
--- ni idea
+select d.nombre as nombre_departamento, count(p.dni) as total_profesores
+from profesor p
+join departamento d on p.cod_dpto = d.cod_dpto
+group by d.nombre
+order by total_profesores desc
+limit 1;
 
 -- Encuentra el nombre y apellidos de los profesores que imparten módulos en el primer curso.
-
 select distinct concat_ws(' ', p.nombre, p.prApellido, p.sgApellido) as Nombre_Completo
 from profesor p
 join contrato c  on p.dni = c.dni_profesor
@@ -88,7 +108,7 @@ where numHoras > 200;
 -- Encuentra el nombre y apellidos de los profesores que no tienen asignado un correo electrónico.
 select concat_ws(' ', nombre, prApellido, sgApellido) as nombre_completo
 from profesor 
-where mail is null;
+where mail is  null;
 
 -- Muestra el nombre del ciclo y el nombre de la familia correspondiente.
 select c.nombre as nombre_ciclo, 
@@ -108,28 +128,82 @@ join calificacion c on m.cod_modulo = c.cod_modulo
 join matricula ma on c.matricula = ma.matricula
 join alumno a on ma.dni_alumno = a.dni;
 
--- Encuentra el nombre del profesor que imparte el módulo con el código 3.
-select p.nombre as nombre_profesor -- Falta el nom completo
-from profesor p 
+-- Muestra el número total de horas de cada ciclo. Debe mostrar el ciclo y las horas totales.
+select c.nombre as Nombre_ciclo, sum(m.numhoras) as Horas_totales 
+from ciclo c
+join modulo m on c.cod_ciclo = m.cod_ciclo
+group by c.nombre;
+
+-- Lista los módulos ordenados por nombre.
+select nombre as Nombre_modulo
+from modulo
+order by nombre asc;
+
+-- Encuentra el nombre y apellidos de los profesores que imparten módulos en el segundo curso.
+select concat_ws(' ',p.nombre, p.prApellido, p.sgApellido) as nombre_completo, m.nombre as modulo
+from profesor p
 join contrato c on p.dni = c.dni_profesor
 join contrato_modulo cm on c.cod_contrato = cm.cod_contrato
-join modulo m on cm.cod_modulo = m.cod_modulo
-where m.cod_modulo = '3';
--- Muestra el nombre del profesor director de cada departamento.
--- Lista los nombres de los alumnos que nacieron después del año 2000.
--- Cuenta el numero de módulos impartidos por cada profesor.
--- Muestra el número total de horas de cada ciclo. Debe mostrar el ciclo y las horas totales.
--- Lista los módulos ordenados por nombre.
--- Encuentra el nombre y apellidos de los profesores que imparten módulos en el segundo curso.
+join modulo m on cm.cod_modulo = m.cod_modulo 
+where m.curso = '2';
+
 -- Muestra el número de módulos impartidos en cada ciclo. Debe aparecer el ciclo y el total de módulos.
+SELECT c.nombre AS nombre_ciclo, COUNT(m.cod_modulo) AS total_modulos
+FROM CICLO c
+JOIN MODULO m ON c.cod_ciclo = m.cod_ciclo
+GROUP BY c.nombre; -- duda
+
+
 -- Encuentra los alumnos que la ultima titulacion sea 'CFGM'.
+select nombre 
+from alumno 
+where ultima_titulacion = 'CFGM';
+
 -- Encuentra los alumnos que la ultima titulacion sea 'CFGM' o 'BACH'. Ordena por ultima titulacion
+select *
+from alumno a
+where a.ultima_titulacion = 'CFGM' or 'BACH'
+order by ultima_titulacion;  -- no me va
+
 -- Cuenta el numero de alumnos existentes por cada ultima titulacion.
+select a.ultima_titulacion, count(*) as alumnos_existentes
+from alumno a 
+group by ultima_titulacion;
+
 -- Lista los módulos que tienen una nota superior a 8.
+select m.nombre as nombre_modulo, c.nota
+from modulo m
+join calificacion c on m.cod_modulo = c.cod_modulo
+where nota > 8;
+
 -- Muestra el nombre y apellidos de los profesores que tienen un correo electrónico.
+select concat_ws(' ',p.nombre, p.prApellido, p.sgApellido) as Nombre_completo
+from profesor p
+where p.mail is null;
+
 -- Lista los nombres de los profesores que imparten módulos en el ciclo DAM.
+select distinct concat_ws(' ',p.nombre, p.prApellido, p.sgApellido) as Nombre_completo
+from profesor p
+join contrato c on p.dni = c.dni_profesor
+join contrato_modulo cm on c.cod_contrato = cm.cod_contrato
+join modulo m on cm.cod_modulo = m.cod_modulo 
+join ciclo cic on m.cod_ciclo = cic.cod_ciclo
+where cic.sigla = 'DAM';
 -- Muestra el nombre completo del profesor que tiene el contrato con el código 5.
 -- Lista los módulos y las notas obtenidas por los alumnos en orden descendente de nota.
 -- Calcula la media de todas las calificaciones existentes.
 -- Calcula la media por módulo
 -- Calcula la media por matricula.
+
+select  m.curso, cl.nombre as Nombre_modulo, concat_ws(' ', p.nombre, p.prApellido, p.sgApellido) as nombre_completo, count(cm.cod_modulo) as total_modulos
+from profesor p
+join contrato c on p.dni = c.dni_profesor
+join contrato_modulo cm on cm.cod_contrato = c.cod_contrato
+join modulo m on cm.cod_modulo = m.cod_modulo
+join ciclo cl on m.cod_ciclo = cl.cod_ciclo
+group by m.curso, cl.cod_ciclo, nombre_completo;  -- todo lo que agrupes hay que meterlo en el group by
+
+
+
+
+
